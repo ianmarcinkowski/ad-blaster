b044ab6c3efb2e67b43104075de5ca2ac67ce897
Ian Marcinkowski <ian@marcinkowski.ca>
2024-10-31 16:18:53 -0400

    Add colour output, mocked mute/unmute implementation

--- ad_blaster/ad_blaster.py
+++ ad_blaster/ad_blaster.py
@@ -2,10 +2,35 @@
 
 import cv2
 from ollama import Client as OllamaClient
+from enum import Enum
+from rich import print as print_rich
 from rich.console import Console
 from rich.markdown import Markdown
 from ad_blaster.prompts import engineered_prompt, user_prompt
 from ad_blaster.util import ns_to_ms, find_json, b64encode
+
+
+class State(Enum):
+    UNMUTED = "unmuted"
+    MUTED = "muted"
+
+
+categories = [
+    "Sports",
+    "Drama",
+    "Comedy",
+    "Advertisement",
+    "Blank Screen",
+    "Sci-fi",
+    "Unknown",
+    "News",
+]
+
+bad_categories = [
+    "Advertisement",
+]
+
+good_categories = ["Sports", "Drama", "Comedy", "Blank Screen", "Sci-fi", "News"]
 
 
 class AdBlaster:
@@ -15,7 +40,16 @@ class AdBlaster:
         self.llm_hostname = self.config["ollama_uri"]
         self.ollama = OllamaClient(host=self.llm_hostname)
         self.console = Console()
-        self.state = "init"
+        self.state = State.UNMUTED
+
+    async def update_state(self, desired_state):
+        previous_state = self.state
+        if previous_state != desired_state:
+            if desired_state == State.UNMUTED:
+                await self.send_unmute()
+            elif desired_state == State.MUTED:
+                await self.send_mute()
+            self.state = desired_state
 
     async def run(self):
         cap = setup_capture(self.config["webcam"]["width"], self.config["webcam"]["height"])
@@ -39,14 +73,34 @@ class AdBlaster:
         frame_b64 = b64encode(buffer)
         length = len(frame_b64)
         response = await self.ask(frame_b64)
+        output = ""
         if response["json"]:
-            self.console.print(response["json"])
+            output = response["json"]
+            decision = self.decide(response["json"])
+            self.console.print(f"[yellow] decision is {decision}")
+            if decision == State.MUTED:
+                await self.update_state(State.MUTED)
+            elif decision == State.UNMUTED:
+                await self.update_state(State.UNMUTED)
+            else:
+                self.console.print("[yellow]Not enough signal to determine whether to mute or not")
         else:
             try:
                 output = Markdown(response["raw_message"])
             except:
                 output = response["raw_message"]
-            self.console.print(output)
+        self.console.print(f"[blue]LLM: {output}")
+
+    def decide(self, response):
+        category = response.get("category")
+        description = response.get("description")
+        decision = None
+        if category is not None:
+            if category in bad_categories:
+                decision = State.MUTED
+            elif category in good_categories:
+                decision = State.UNMUTED
+        return decision
 
     async def ask(self, image_content):
         response = self.ollama.chat(
@@ -62,6 +116,12 @@ class AdBlaster:
             "raw_message": response["message"],
             "json": found_json,
         }
+
+    async def send_unmute(self):
+        self.console.print("[red]PLACEHOLDER UNMUTE COMMAND SENT")
+
+    async def send_mute(self):
+        self.console.print("[red]PLACEHOLDER MUTE COMMAND SENT")
 
 
 def setup_capture(x_size, y_size, device_id=0):
